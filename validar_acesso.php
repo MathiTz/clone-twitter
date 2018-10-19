70bc15eb155cfeafb7fba560fbbe129801712dc1
mathitz <matheusalves789@gmail.com>
2018-10-19 11:36:18 -0300

    Adicionado código para página sair e redirecionamento para index, além de codificação para sessão

--- validar_acesso.php
+++ validar_acesso.php
@@ -1,12 +1,14 @@
 <?php
 
+session_start();
+
 require_once('db.class.php');
 
 $usuario = $_POST['usuario'];
 
-$senha = $_POST['senha'];
+$senha = md5($_POST['senha']);
 
-$sql = " SELECT * FROM usuarios WHERE usuario = '$usuario' AND senha = '$senha' ";
+$sql = " SELECT usuario, email FROM usuarios WHERE usuario = '$usuario' AND senha = '$senha' ";
 
 $objDb = new db();
 $link = $objDb->conecta_mysql();
@@ -19,7 +21,10 @@ if($resultado_id)
 
     if(isset($dados_usuario['usuario']))
     {
-        echo 'usuário existe';
+        $_SESSION['usuario'] = $dados_usuario['usuario'];
+        $_SESSION['email'] = $dados_usuario['email'];
+
+        header('Location: home.php');
     } else
     {
         header('Location: index.php?erro=1');
