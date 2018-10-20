6249052a7c0106b2fe5dfa3ec657ed3585973e5c
mathitz <matheusalves789@gmail.com>
2018-10-20 01:40:04 -0300

    Tratativa de autenticação a partir de consulta no db

--- registra_usuario.php
+++ registra_usuario.php
@@ -11,6 +11,47 @@ $senha = md5($_POST['senha']);
 $objDb = new db();
 $link = $objDb->conecta_mysql();
 
+//verificar se o usurário já existe
+
+$sql = "select * from usuarios where usuario = '$usuario'";
+if($resultado_id = mysqli_query($link, $sql))
+{
+    $dados_usuario = mysqli_fetch_array($resultado_id);
+    
+    if(isset($dados_usuario['usuario']))
+    {
+        echo 'Usuário já cadastrado';
+    } else
+    {
+        echo 'Usuário cadastrado com sucesso!';
+    }
+
+}else
+{
+    echo 'Erro ao tentar localizar o registro de usuário';
+};
+
+//verificar se o email já existe
+
+$sql = "select * from usuarios where email = '$email'";
+if($resultado_id = mysqli_query($link, $sql))
+{
+    $dados_usuario = mysqli_fetch_array($resultado_id);
+    
+    if(isset($dados_usuario['email']))
+    {
+        echo '<br>Email já cadastrado';
+    } else
+    {
+        echo 'Email cadastrado com sucesso!';
+    }
+
+}else
+{
+    echo 'Erro ao tentar localizar o registro de usuário';
+};
+
+die();
 $sql = " insert into usuarios (usuario, email, senha) values ('$usuario', '$email', '$senha')";
 
 //execução da query
